1974cd7dbcc0e138a429365aee14f1d4491ebb85
nitish <nitishvibhav@gmail.com>
2022-03-15 10:15:56 +0530

    add dbms

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
-const links = document.querySelectorAll('.link');
-const sections = document.querySelectorAll('section');
+var express=require("express");
+var bodyParser=require("body-parser");
 
-let activeLink = 0;
-
-links.forEach((link, i) => {
-    link.addEventListener('click', () => {
-        if(activeLink != i){
-            links[activeLink].classList.remove('active');
-            link.classList.add('active');
-            sections[activeLink].classList.remove('active');
-
-            setTimeout(() => {
-                activeLink = i;
-                sections[i].classList.add('active');
-            }, 1000);
-        }
-    })
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/signup');
+var db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+   console.log("connection succeeded");
 })
+var app=express()
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+
+app.post('/contact-form', function(req,res){
+   var name = req.body.name;
+   var email =req.body.email;
+   var msg = req.body.msg;
+
+
+   var data = {
+      "name": name,
+      "email":email,
+      "msg":msg
+   }
+   db.collection('message').insertOne(data,function(err, collection){
+   if (err) throw err;
+      console.log("Record inserted Successfully");
+   });
+   return res.send('message sent successfully');
+})
+
+app.get('/',function(req,res){
+   res.set({
+      'Access-control-Allow-Origin': '*'
+   });
+   return res.redirect('index.html');
+}).listen(3000)
+
+console.log("server listening at port 3000");
